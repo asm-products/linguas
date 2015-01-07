772740114a09bef5ca1c1fd9cf073e6eebb46f3d
BonDoQ <ahmed@bondoq.me>
2015-01-07 14:33:42 +0200

    add README.md File && serve files using grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,97 +1,107 @@
 var path = require("path");
 
 var lessCreateConfig = function (context, block) {
-  var cfg = {files: []},
-    outfile = path.join(context.outDir, block.dest),
-    filesDef = {};
+    var cfg = {files: []},
+        outfile = path.join(context.outDir, block.dest),
+        filesDef = {};
 
-  filesDef.dest = outfile;
-  filesDef.src = [];
+    filesDef.dest = outfile;
+    filesDef.src = [];
 
-  context.inFiles.forEach(function (inFile) {
-    filesDef.src.push(path.join(context.inDir, inFile));
-  });
+    context.inFiles.forEach(function (inFile) {
+        filesDef.src.push(path.join(context.inDir, inFile));
+    });
 
-  cfg.files.push(filesDef);
-  context.outFiles = [block.dest];
-  return cfg;
+    cfg.files.push(filesDef);
+    context.outFiles = [block.dest];
+    return cfg;
 };
 
 module.exports = function (grunt) {
 
-  require('load-grunt-tasks')(grunt);
+    require('load-grunt-tasks')(grunt);
 
-  grunt.config.init({
-    useminPrepare: {
-      html: 'index.html',
-      options: {
-        dest: 'dist',
-        flow: {
-          steps: {
-            'js': ['concat'],
-            'css': ['concat', 'cssmin'],
-            'less': [
-              {
-                name: 'less',
-                createConfig: lessCreateConfig
-              }
-            ]
-          },
-          post: {}
+    grunt.config.init({
+        useminPrepare: {
+            html: 'index.html',
+            options: {
+                dest: 'dist',
+                flow: {
+                    steps: {
+                        'js': ['concat'],
+                        'css': ['concat', 'cssmin'],
+                        'less': [
+                            {
+                                name: 'less',
+                                createConfig: lessCreateConfig
+                            }
+                        ]
+                    },
+                    post: {}
+                }
+            }
+        },
+        usemin: {
+            html: 'dist/index.html',
+            options: {
+                blockReplacements: {
+                    less: function (block) {
+                        return '<link rel="stylesheet" href="' + block.dest + '">';
+                    }
+                }
+            }
+        },
+        copy: {
+            index: {
+                src: ['index.html'],
+                dest: 'dist/'
+            },
+            manifest: {
+                src: ['manifest.json'],
+                dest: 'dist/'
+            },
+            maps: {
+                expand: true,
+                flatten: true,
+                src: ['bower_components/angular/angular.min.js.map', 'bower_components/angular-route/angular-route.min.js.map'],
+                dest: 'dist/app/js/'
+            },
+            templates: {
+                expand: true,
+                src: ['app/features/**/*.html'],
+                dest: 'dist/'
+            },
+            assets: {
+                expand: true,
+                src: ['app/assets/img/**/*.*'],
+                dest: 'dist/'
+            },
+            flatui: {
+                expand: true,
+                flatten: true,
+                src: ['bower_components/flat-ui/fonts/glyphicons/flat-ui-icons-regular.*'],
+                dest: 'dist/app/fonts/glyphicons/'
+            }
+        },
+        'http-server': {
+            dev: {
+                root: 'dist/',
+                port: '8080',
+                host: 'localhost',
+                autoIndex: true,
+                ext: 'html'
+            }
         }
-      }
-    },
-    usemin: {
-      html: 'dist/index.html',
-      options: {
-        blockReplacements: {
-          less: function (block) {
-            return '<link rel="stylesheet" href="' + block.dest + '">';
-          }
-        }
-      }
-    },
-    copy: {
-      index: {
-        src: ['index.html'],
-        dest: 'dist/'
-      },
-      manifest: {
-        src: ['manifest.json'],
-        dest: 'dist/'
-      },
-      maps: {
-        expand: true,
-        flatten: true,
-        src: ['bower_components/angular/angular.min.js.map', 'bower_components/angular-route/angular-route.min.js.map'],
-        dest: 'dist/app/js/'
-      },
-      templates: {
-        expand: true,
-        src: ['app/features/**/*.html'],
-        dest: 'dist/'
-      },
-      assets: {
-        expand: true,
-        src: ['app/assets/img/**/*.*'],
-        dest: 'dist/'
-      },
-      flatui: {
-        expand: true,
-        flatten: true,
-        src: ['bower_components/flat-ui/fonts/glyphicons/flat-ui-icons-regular.*'],
-        dest: 'dist/app/fonts/glyphicons/'
-      }
-    }
-  });
+    });
 
+    grunt.loadNpmTasks('grunt-http-server');
 
-  grunt.registerTask('default', [
-    'useminPrepare',
-    'concat',
-    'less',
-    'cssmin',
-    'copy',
-    'usemin'
-  ]);
+    grunt.registerTask('default', [
+        'useminPrepare',
+        'concat',
+        'less',
+        'cssmin',
+        'copy',
+        'usemin'
+    ]);
 }
---
+++ README.md
@@ -0,0 +1,16 @@
+
+#Linguas
+
+A web site where we practice languages by learning a new sentence every day. One English sentence is added every morning and the translations in other languages are added by the users voluntarily. Therefore, while users practice languages they also help others to learn. If you want, you can also contribute by adding translations in the languages you speak.
+
+
+## Setup
+
+* clone Repo
+* run `npm install`
+* run  `bower install`
+* run  `grunt`
+* run  `grunt http-server`
+
+The website is now ready from om <b>`http://localhost:8080/` </b>
+
